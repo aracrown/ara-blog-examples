6fde0ebd724d44c20c5d237b4973846b143e5a18
Vytautas R. <turisto@gmail.com>
2015-05-20 23:29:27 +0300

    initial commit.

---
+++ s01e03/src/main/java/org/aracrown/blog/s01e03/model/BlogEntry.java
@@ -0,0 +1,38 @@
+package org.aracrown.blog.s01e03.model;
+
+import javax.persistence.Cacheable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "BLOG_ENTRY")
+@Cacheable
+public class BlogEntry {
+	/** The primary key. */
+	@Id
+	@SequenceGenerator(name = "BLOG_ENTRY_ID_GENERATOR", sequenceName = "BLOG_ENTRY_SEQ")
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BLOG_ENTRY_ID_GENERATOR")
+	@Column(name = "ID", unique = true, nullable = false)
+	private Long id;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	
+}
