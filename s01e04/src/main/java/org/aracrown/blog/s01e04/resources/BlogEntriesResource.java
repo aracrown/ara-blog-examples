20ae5e65c28df47afd3d0397dee3400243586462
Vytautas R. <turisto@gmail.com>
2015-06-05 22:36:17 +0300

    minor updates.

--- s01e04/src/main/java/org/aracrown/blog/s01e04/resources/BlogEntriesResource.java
+++ s01e04/src/main/java/org/aracrown/blog/s01e04/resources/BlogEntriesResource.java
@@ -11,7 +11,7 @@ import org.aracrown.blog.s01e04.dao.BlogEntryDao;
 import org.aracrown.blog.s01e04.model.BlogEntry;
 import org.aracrown.commons.rest.ResourceCollectionResponse;
 
-@Path("/")
+@Path("/blogs/")
 @Produces({ MediaType.APPLICATION_JSON })
 @Consumes({ MediaType.APPLICATION_JSON })
 public class BlogEntriesResource {
@@ -21,8 +21,6 @@ public class BlogEntriesResource {
 
 	@GET
 	public ResourceCollectionResponse<BlogEntry> getAll() {
-
-		return new BlogEntryResourceCollectionTransformation(new ResourceCollectionResponse<BlogEntry>()).total(blogEntryDao.count()).page(0L, 10L)
-				.transform(blogEntryDao.page(0L, 10L));
+		return new BlogEntryResourceCollectionTransformation(0L, 10L).total(blogEntryDao.count()).transform(blogEntryDao.page(0L, 10L));
 	}
 }
--- s01e04/src/main/java/org/aracrown/blog/s01e04/resources/BlogEntryResourceCollectionTransformation.java
+++ s01e04/src/main/java/org/aracrown/blog/s01e04/resources/BlogEntryResourceCollectionTransformation.java
@@ -4,11 +4,11 @@ import org.aracrown.blog.s01e04.model.BlogEntry;
 import org.aracrown.commons.rest.ResourceCollectionResponse;
 import org.aracrown.commons.rest.ResourceCollectionTransformation;
 
-public class BlogEntryResourceCollectionTransformation extends ResourceCollectionTransformation<BlogEntry, BlogEntry>{
+public class BlogEntryResourceCollectionTransformation extends ResourceCollectionTransformation<BlogEntry, BlogEntry> {
 
-	public BlogEntryResourceCollectionTransformation(ResourceCollectionResponse<BlogEntry> target) {
-		super(target);
-		page(0L, 10L);
+	public BlogEntryResourceCollectionTransformation(Long first, Long pageSize) {
+		super(new ResourceCollectionResponse<BlogEntry>());
+		page(first, pageSize);
 	}
 
 	@Override
